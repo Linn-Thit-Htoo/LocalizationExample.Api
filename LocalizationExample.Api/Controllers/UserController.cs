00b0d47adc84de0977a2664e63152194ee046d2b
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-17 18:50:02 +0630

    Add project files.

---
+++ LocalizationExample.Api/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using LocalizationExample.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace LocalizationExample.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        internal readonly IStringLocalizer<UserController> _localizer;
+
+        public UserController(IStringLocalizer<UserController> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(UserRequestModel requestModel)
+        {
+            if (string.IsNullOrEmpty(requestModel.UserName))
+            {
+                string errorMessage = _localizer["UserName"].Value;
+                return BadRequest(errorMessage);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ LocalizationExample.Api/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+var cultures = new List<CultureInfo> {
+    new CultureInfo("en"),
+    new CultureInfo("fr"),
+    new CultureInfo("my-MM")
+};
+
+app.UseRequestLocalization(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture("en");
+    options.SupportedCultures = cultures;
+    options.SupportedUICultures = cultures;
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
