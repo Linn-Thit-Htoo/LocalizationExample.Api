6947365bed38054097b9ec50c02a9847310242b2
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-18 08:47:33 +0630

    -

--- LocalizationExample.Api/Validators/UserValidator.cs
+++ LocalizationExample.Api/Validators/UserValidator.cs
@@ -3,16 +3,15 @@ using LocalizationExample.Api.Controllers;
 using LocalizationExample.Api.Models;
 using Microsoft.Extensions.Localization;
 
-namespace LocalizationExample.Api.Validators
+namespace LocalizationExample.Api.Validators;
+
+public class UserValidator : AbstractValidator<UserRequestModel>
 {
-    public class UserValidator : AbstractValidator<UserRequestModel>
+    private readonly IStringLocalizer<UserController> _localizer;
+    public UserValidator(IStringLocalizer<UserController> localizer)
     {
-        private readonly IStringLocalizer<UserController> _localizer;
-        public UserValidator(IStringLocalizer<UserController> localizer)
-        {
-            _localizer = localizer; 
-            RuleFor(x => x.UserName).NotEmpty().WithMessage(_localizer["UserNameNotEmpty"].Value).NotNull().WithMessage(_localizer["UserNameNotEmpty"].Value);
-            RuleFor(x => x.Email).NotEmpty().WithMessage(_localizer["EmailNotEmpty"].Value).NotNull().WithMessage(_localizer["EmailNotEmpty"].Value);
-        }
+        _localizer = localizer; 
+        RuleFor(x => x.UserName).NotEmpty().WithMessage(_localizer["UserNameNotEmpty"].Value).NotNull().WithMessage(_localizer["UserNameNotEmpty"].Value);
+        RuleFor(x => x.Email).NotEmpty().WithMessage(_localizer["EmailNotEmpty"].Value).NotNull().WithMessage(_localizer["EmailNotEmpty"].Value);
     }
 }
