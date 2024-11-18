ac3120c41b5dc6b86cccdbb6749a193cb7393bce
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-18 08:48:48 +0630

    -

--- LocalizationExample.Api/Validators/UserValidator.cs
+++ LocalizationExample.Api/Validators/UserValidator.cs
@@ -8,10 +8,19 @@ namespace LocalizationExample.Api.Validators;
 public class UserValidator : AbstractValidator<UserRequestModel>
 {
     private readonly IStringLocalizer<UserController> _localizer;
+
     public UserValidator(IStringLocalizer<UserController> localizer)
     {
-        _localizer = localizer; 
-        RuleFor(x => x.UserName).NotEmpty().WithMessage(_localizer["UserNameNotEmpty"].Value).NotNull().WithMessage(_localizer["UserNameNotEmpty"].Value);
-        RuleFor(x => x.Email).NotEmpty().WithMessage(_localizer["EmailNotEmpty"].Value).NotNull().WithMessage(_localizer["EmailNotEmpty"].Value);
+        _localizer = localizer;
+        RuleFor(x => x.UserName)
+            .NotEmpty()
+            .WithMessage(_localizer["UserNameNotEmpty"].Value)
+            .NotNull()
+            .WithMessage(_localizer["UserNameNotEmpty"].Value);
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage(_localizer["EmailNotEmpty"].Value)
+            .NotNull()
+            .WithMessage(_localizer["EmailNotEmpty"].Value);
     }
 }
